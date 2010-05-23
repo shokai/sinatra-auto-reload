c98dc6c70bb6e5890f473b1bb54902e135d43957
Sho Hashimoto <hashimoto@shokai.org>
2010-05-23 23:47:41 +0900

    add examples

---
+++ examples/app.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'sinatra'
+require 'rack'
+require 'sinatra-auto-reload' if development?
+#require '../lib/sinatra-auto-reload' if development?
+
+
+get '/' do
+  @host = env['HTTP_HOST']
+  if development?
+    @mes = erb %{
+hello world<br />
+install <a href="http://<%=@host%>/sinatra_auto_reload.user.js">userscript</a><br />
+it automatically reload your web browser
+  }
+  else
+    @mes = 'ruby app.rb -e development<br />or<br />shotgun app.rb'
+  end
+end
+
+
+
